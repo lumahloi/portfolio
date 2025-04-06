66b6aec41f87de45d6863ee22b56b6de1493a6c6
lumahloi <magusoflowers0803@gmail.com>
2025-04-06 13:56:14 -0300

    add projects json structure

--- src/pages/Portfolio/sections/Projetos/index.js
+++ src/pages/Portfolio/sections/Projetos/index.js
@@ -1,37 +1,56 @@
-import MKBox from "components/MKBox";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 import TransparentBlogCard from "examples/Cards/BlogCards/TransparentBlogCard";
-import bgImage from "assets/images/bg-cats.gif";
+
+import bgCats from "assets/images/bg-cats.gif";
+
+const projetos = [
+  {
+    id: 1,
+    title: "Rover raised $65 million",
+    description: "Finding temporary housing for your dog...",
+    image: bgCats, // Remova as chaves {} - use a referência diretamente
+    tags: ["finanças", "startup"]
+  },
+  {
+    id: 2,
+    title: "Rover raised $65 million",
+    description: "Finding temporary housing for your dog...",
+    image: bgCats, // Remova as chaves {} - use a referência diretamente
+    tags: ["finanças", "startup"]
+  }
+];
 
 export default function Projetos() {
-    return (
-        <>
-            <MKBox component="section" py={2}>
-                <Container>
-                    <Grid container item xs={12} lg={6}>
-                        <MKTypography variant="h3" mb={6}>
-                            Meus projetos
-                        </MKTypography>
-                    </Grid>
-                    <Grid container spacing={5} justifyContent="space-around">
-                        <Grid item xs={12} md={6} sm={12} lg={4}>
-                            <TransparentBlogCard
-                                image={bgImage}
-                                title="Rover raised $65 million"
-                                description="Finding temporary housing for your dog should be as easy as renting an Airbnb. That’s the idea behind Rover ..."
-                                action={{
-                                    type: "internal",
-                                    route: "/pages/blogs/author",
-                                    color: "info",
-                                    label: "read more",
-                                }}
-                            />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </MKBox>
-        </>
-    )
+  return (
+    <MKBox component="section" py={2}>
+      <Container>
+        <Grid container item xs={12} lg={6}>
+          <MKTypography variant="h3" mb={6}>
+            Meus projetos
+          </MKTypography>
+        </Grid>
+
+        <Grid container spacing={3}>
+          {projetos.map((projeto) => (
+            <Grid item xs={12} md={6} lg={4} key={projeto.id}>
+              <TransparentBlogCard
+                image={projeto.image}
+                title={projeto.title}
+                description={projeto.description}
+                action={{
+                  type: "internal",
+                  route: `/projetos/${projeto.id}`,
+                  color: "info",
+                  label: "ver detalhes",
+                }}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </MKBox>
+  );
 }
