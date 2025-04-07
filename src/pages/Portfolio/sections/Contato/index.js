dd1b42c028c14a96f8a4432f99ce2b09bf46c8a6
lumahloi <magusoflowers0803@gmail.com>
2025-04-06 21:24:02 -0300

    fix contact

--- src/pages/Portfolio/sections/Contato/index.js
+++ src/pages/Portfolio/sections/Contato/index.js
@@ -2,7 +2,8 @@ import MKBox from "components/MKBox";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import MKTypography from "components/MKTypography";
-import MKButton from "components/MKButton";
+// import MKButton from "components/MKButton";
+import MKSocialButton from "components/MKSocialButton";
 import bgImage from "assets/images/bg-cats.gif";
 
 export default function Contato() {
@@ -30,46 +31,42 @@ export default function Contato() {
                     <Container sx={{ position: "relative", zIndex: 2, py: 12 }}>
                         <Grid container item xs={12} md={7} justifyContent="center" mx="auto" textAlign="center">
                             <MKTypography variant="h3" color="white">
-                                Do you love this awesome
+                                Vamos desenvolver algo incrível!
                             </MKTypography>
-                            <MKTypography variant="h3" color="white" mb={1}>
-                                UI Kit for ReactJS &amp; MUI?
+                            <MKTypography variant="body2" color="white" mb={3} mt={2}>
+                                Entre em contato através das formas listadas abaixo e vamos colaborar em projetos.
                             </MKTypography>
-                            <MKTypography variant="body2" color="white" mb={6}>
-                                Cause if you do, it can be yours for FREE. Hit the button below to navigate to
-                                Creative Tim where you can find the Design System in HTML. Start a new project or give
-                                an old Bootstrap project a new look!
-                            </MKTypography>
-                            <MKButton
-                                variant="gradient"
-                                color="info"
-                                size="large"
-                                component="a"
-                                href="https://www.creative-tim.com/product/material-kit-react"
-                                sx={{ mb: 2 }}
-                            >
-                                Download Now
-                            </MKButton>
-                            <MKButton
-                                variant="gradient"
-                                color="info"
-                                size="large"
-                                component="a"
-                                href="https://www.creative-tim.com/product/material-kit-react"
-                                sx={{ mb: 2 }}
-                            >
-                                Download Now
-                            </MKButton>
-                            <MKButton
-                                variant="gradient"
-                                color="info"
-                                size="large"
-                                component="a"
-                                href="https://www.creative-tim.com/product/material-kit-react"
-                                sx={{ mb: 2 }}
-                            >
-                                Download Now
-                            </MKButton>
+                            <Grid item>
+                                <MKSocialButton
+                                    component="a"
+                                    href="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23mui5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-kit-react"
+                                    target="_blank"
+                                    color="twitter"
+                                    sx={{ mr: 1 }}
+                                >
+                                    <i className="fab fa-twitter" />
+                                    &nbsp;Tweet
+                                </MKSocialButton>
+                                <MKSocialButton
+                                    component="a"
+                                    href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-kit-react"
+                                    target="_blank"
+                                    color="facebook"
+                                    sx={{ mr: 1 }}
+                                >
+                                    <i className="fab fa-facebook" />
+                                    &nbsp;Share
+                                </MKSocialButton>
+                                <MKSocialButton
+                                    component="a"
+                                    href="https://www.pinterest.com/pin/create/button/?url=https://www.creative-tim.com/product/material-kit-react"
+                                    target="_blank"
+                                    color="pinterest"
+                                >
+                                    <i className="fab fa-pinterest" />
+                                    &nbsp;Pin it
+                                </MKSocialButton>
+                            </Grid>
                         </Grid>
                     </Container>
                 </MKBox>
