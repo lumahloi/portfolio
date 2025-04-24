d0e7008c82fdfb574b141fd841db404ce8da40f0
lumahloi <magusoflowers0803@gmail.com>
2025-04-24 19:08:22 -0300

    fix particles

--- src/assets/theme/base/colors.js
+++ src/assets/theme/base/colors.js
@@ -118,18 +118,8 @@ export default {
   },
 
   socialMediaColors: {
-    facebook: { main: "#3b5998", dark: "#344e86" },
-    twitter: { main: "#55acee", dark: "#3ea1ec" },
-    instagram: { main: "#125688", dark: "#0e456d" },
     linkedin: { main: "#0077b5", dark: "#00669c" },
-    pinterest: { main: "#cc2127", dark: "#b21d22" },
-    youtube: { main: "#e52d27", dark: "#d41f1a" },
-    vimeo: { main: "#1ab7ea", dark: "#13a3d2" },
-    slack: { main: "#3aaf85", dark: "#329874" },
-    dribbble: { main: "#ea4c89", dark: "#e73177" },
-    github: { main: "#ffffff", dark: "#bbbbbb" },
-    reddit: { main: "#ff4500", dark: "#e03d00" },
-    tumblr: { main: "#35465c", dark: "#2a3749" },
+    github: { main: "#000000", dark: "#000000" },
   },
 
   badgeColors: {
--- src/pages/Portfolio/sections/Sobre/index.js
+++ src/pages/Portfolio/sections/Sobre/index.js
@@ -1,18 +1,10 @@
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
-import MKSocialButton from "components/MKSocialButton";
-import MKButton from "components/MKButton";
 
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import DownloadIcon from "@mui/icons-material/Download";
-import MailIcon from "@mui/icons-material/Mail";
 
 import profileImage from "assets/images/profile-img.png";
-import cv from "assets/cv/curriculo.pdf";
-
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 import { useTranslation } from "react-i18next";
 
@@ -28,233 +20,145 @@ export default function Sobre() {
       <MKBox component="section" py={12}>
         <Container>
           <Grid container spacing={2} alignItems="center">
-            <Grid item xs={12} lg={4} md={12} sx={{ ml: "auto", mt: { xs: 3, lg: 0 } }}>
-              <MKBox
-                position="relative"
-                borderRadius="lg"
-                mx={2}
-                mt={-3}
-                sx={{
-                  overflow: "visible",
-                  padding: "20px",
-                  border: "none",
+            <Grid container spacing={2} alignItems="center" sx={{ position: "relative" }}>
+              <div
+                style={{
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                  top: 0,
+                  left: 0,
+                  zIndex: 0,
                 }}
               >
-                <div
-                  style={{
-                    position: "absolute",
-                    width: "calc(100% + 160px)",
-                    height: "calc(100% + 160px)",
-                    top: "-80px",
-                    left: "-80px",
-                    zIndex: 0,
-                  }}
-                >
-                  <Particles
-                    id="tsparticles"
-                    init={useCallback(async (engine) => {
-                      await loadSlim(engine);
-                    }, [])}
-                    options={{
-                      fullScreen: { enable: false },
-                      background: {
-                        color: "transparent",
-                      },
-                      fpsLimit: 120,
-                      interactivity: {
-                        events: {
-                          onHover: {
-                            enable: true,
-                            mode: "repulse",
-                          },
-                        },
-                        modes: {
-                          repulse: {
-                            distance: 100,
-                            duration: 0.4,
-                          },
-                        },
-                      },
-                      particles: {
-                        color: {
-                          value: "#ffffff",
-                        },
-                        links: {
-                          color: "#ffffff",
-                          distance: 150,
+                <Particles
+                  id="tsparticles"
+                  init={useCallback(async (engine) => {
+                    await loadSlim(engine);
+                  }, [])}
+                  options={{
+                    fullScreen: { enable: false },
+                    background: {
+                      color: "transparent",
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                      events: {
+                        onHover: {
                           enable: true,
-                          opacity: 0.5,
-                          width: 1,
-                        },
-                        move: {
-                          enable: true,
-                          speed: 2,
-                        },
-                        number: {
-                          density: {
-                            enable: true,
-                          },
-                          value: 80,
-                        },
-                        opacity: {
-                          value: 0.5,
-                        },
-                        shape: {
-                          type: "circle",
-                        },
-                        size: {
-                          value: { min: 1, max: 3 },
+                          mode: "repulse",
                         },
                       },
-                    }}
-                  />
-                </div>
-
-                <MKBox
-                  component="img"
-                  src={profileImage}
-                  alt="Lumah Pereira em Pixel Art"
-                  borderRadius="lg"
-                  width="100%"
-                  position="relative"
-                  zIndex={1}
-                  sx={{
-                    boxShadow: "0 4px 20px 0 rgba(0, 0, 0, 0.25)",
+                      modes: {
+                        repulse: {
+                          distance: 100,
+                          duration: 0.4,
+                        },
+                      },
+                    },
+                    particles: {
+                      color: {
+                        value: "#ffffff",
+                      },
+                      links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                      },
+                      move: {
+                        enable: true,
+                        speed: 2,
+                      },
+                      number: {
+                        density: {
+                          enable: true,
+                        },
+                        value: 80,
+                      },
+                      opacity: {
+                        value: 0.5,
+                      },
+                      shape: {
+                        type: "circle",
+                      },
+                      size: {
+                        value: { min: 1, max: 3 },
+                      },
+                    },
                   }}
                 />
-              </MKBox>
-            </Grid>
-            <Grid item xs={12} lg={6} sx={{ ml: "auto", mt: { xs: 3, lg: 0 } }}>
-              <Grid container justifyContent="flex-start">
-                <Grid item xs={12} md={12} lg={12}>
-                  <MKBox mb={12} lg={12}>
-                    <MKTypography variant="h3" mb={3}>
-                      {t("about-title")}
-                    </MKTypography>{" "}
-                    <Grid container>
-                      <MKTypography variant="body1" mb={3}>
-                        {" "}
-                        {t("about-txt-1")}
-                      </MKTypography>
-                      <MKTypography variant="body1" mb={3}>
-                        {" "}
-                        {t("about-txt-2")}
-                      </MKTypography>
-                      <MKTypography variant="body1" mb={3}>
-                        {" "}
-                        {t("about-txt-3")}
-                      </MKTypography>
-                      <Grid
-                        container
-                        spacing={1}
-                        alignItems="center"
-                        justifyContent="center"
-                        sx={{ width: "100%" }}
-                        mt={3}
-                      >
-                        <Grid item xs={12} sm="auto">
-                          <MKSocialButton
-                            component="a"
-                            href="https://github.com/lumahloi"
-                            target="_blank"
-                            color="github"
-                            fullWidth
-                            sx={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              py: 1.5,
-                              "& .MuiButton-startIcon": {
-                                margin: 0,
-                                "& > *:nth-of-type(1)": {
-                                  fontSize: "1rem",
-                                  marginBottom: 0,
-                                },
-                              },
-                            }}
-                            startIcon={
-                              <>
-                                <i className="fab fa-github" />
-                                <GitHubIcon sx={{ fontSize: "1rem", ml: 0.5 }} />
-                              </>
-                            }
-                          >
-                            <span style={{ marginLeft: "8px" }}>GitHub</span>
-                          </MKSocialButton>
-                        </Grid>
+              </div>
+              <Grid
+                item
+                xs={12}
+                lg={4}
+                md={12}
+                sx={{ ml: "auto", mt: { xs: 3, lg: 0 }, position: "relative", zIndex: 1 }}
+              >
+                <MKBox
+                  position="relative"
+                  borderRadius="lg"
+                  mx={2}
+                  mt={-3}
+                  sx={{
+                    overflow: "visible",
+                    padding: "20px",
+                    border: "none",
+                  }}
+                >
+                  <MKBox
+                    component="img"
+                    src={profileImage}
+                    alt="Lumah Pereira em Pixel Art"
+                    borderRadius="lg"
+                    width="100%"
+                    position="relative"
+                    zIndex={1}
+                    sx={{
+                      boxShadow: "0 4px 20px 0 rgba(0, 0, 0, 0.25)",
+                    }}
+                  />
+                </MKBox>
+              </Grid>
 
-                        <Grid item xs={12} sm="auto">
-                          <MKSocialButton
-                            component="a"
-                            href="https://www.linkedin.com/in/lumah-pereira/"
-                            target="_blank"
-                            color="linkedin"
-                            fullWidth
-                            sx={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              py: 1.5,
-                              "& .MuiButton-startIcon": {
-                                margin: 0,
-                                "& > *:nth-of-type(1)": {
-                                  fontSize: "1rem",
-                                  marginBottom: 0,
-                                },
-                              },
-                            }}
-                            startIcon={
-                              <>
-                                <i className="fab fa-linkedin" />
-                                <LinkedInIcon sx={{ fontSize: "1rem", ml: 0.5 }} />
-                              </>
-                            }
-                          >
-                            <span style={{ marginLeft: "8px" }}>LinkedIn</span>
-                          </MKSocialButton>
-                        </Grid>
-
-                        <Grid item xs={12} sm="auto">
-                          <MKButton
-                            component="a"
-                            href={cv}
-                            download="curriculo.pdf"
-                            color="primary"
-                            fullWidth
-                            sx={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              py: 1.5,
-                              textDecoration: "none",
-                            }}
-                            startIcon={<DownloadIcon />}
-                          >
-                            <span style={{ marginLeft: "8px" }}>{t("header-btn-2")}</span>
-                          </MKButton>
-                        </Grid>
-
-                        <Grid item xs={12} sm="auto">
-                          <MKButton
-                            component="a"
-                            href="mailto:lumah.pereira26@gmail.com"
-                            color="secondary"
-                            fullWidth
-                            sx={{
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              py: 1.5,
-                              textDecoration: "none",
-                            }}
-                            startIcon={<MailIcon />}
-                          >
-                            <span style={{ marginLeft: "8px" }}>E-mail</span>
-                          </MKButton>
-                        </Grid>
+              <Grid
+                item
+                xs={12}
+                lg={6}
+                sx={{ ml: "auto", mt: { xs: 3, lg: 0 }, position: "relative", zIndex: 1 }}
+              >
+                <Grid container justifyContent="flex-start">
+                  <Grid item xs={12} md={12} lg={12}>
+                    <MKBox mb={12} lg={12}>
+                      <MKTypography variant="h3" mb={3} color="white">
+                        {t("about-title")}
+                      </MKTypography>{" "}
+                      <Grid container>
+                        <MKTypography variant="body1" mb={3} color="white">
+                          {" "}
+                          {t("about-txt-1")}
+                        </MKTypography>
+                        <MKTypography variant="body1" mb={3} color="white">
+                          {" "}
+                          {t("about-txt-2")}
+                        </MKTypography>
+                        <MKTypography variant="body1" mb={3} color="white">
+                          {" "}
+                          {t("about-txt-3")}
+                        </MKTypography>
+                        <Grid
+                          container
+                          spacing={1}
+                          alignItems="center"
+                          justifyContent="center"
+                          sx={{ width: "100%" }}
+                          mt={3}
+                        ></Grid>
                       </Grid>
-                    </Grid>
-                  </MKBox>
+                    </MKBox>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
